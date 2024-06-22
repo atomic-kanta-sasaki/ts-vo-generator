a25f6edf73accf71ad95edc7e0b28794ec9bebe9
atomic-kanta-sasaki <kanta.sasaki.0111@gmail.com>
2024-06-22 17:51:18 +0900

    release v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export { generateValueObjectsFromClass } from './generator';
+#!/usr/bin/env node
+'use strict';
+import { generateValueObjectsFromClass } from './generator';
+import * as path from 'path';
+
+const args = process.argv.slice(2);
+if (args.length !== 2) {
+    console.error('Usage: typescript-value-object-generator <input_file> <output_directory>');
+    process.exit(1);
+}
+
+const [inputFile, outputDir] = args;
+const absoluteInputFile = path.resolve(inputFile);
+const absoluteOutputDir = path.resolve(outputDir);
+
+generateValueObjectsFromClass(absoluteInputFile, absoluteOutputDir);
